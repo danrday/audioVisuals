665552485f64e5aa51f389a5c7b8b5d26a03777c
Dan Day <danday@gmail.com>
2016-07-28 14:25:33 -0500

    moved spotify server and node modules into main directory

---
+++ app/userTokens.js
@@ -0,0 +1,11 @@
+"use strict";
+
+let accessToken = null;
+let refreshToken = null;
+
+exports.setTokens = function(accessTokenFromSpotify, refreshTokenFromSpotify) {
+  accessToken = accessTokenFromSpotify;
+  refreshToken = refreshTokenFromSpotify;
+  console.log("accessToken", accessToken);
+  console.log("refreshToken", refreshToken);
+};
