cf585776b62aafc5878e70759ebc726ab0228cc8
Dan Day <danday@gmail.com>
2016-08-02 11:02:51 -0500

    graph printing bar lengths of songs

--- public/app/controllers/DataCtrl.js
+++ public/app/controllers/DataCtrl.js
@@ -11,66 +11,68 @@ app.controller("DataCtrl", function($scope) {
 
 let trackAnalysis = $scope.trackAnalysis;
 
+
+//track bars OBJECT, if over 99 broken into objects of 99 or less
 let trackBars = trackAnalysis.bars;
 
 let trackBarsLength = trackBars.length;
 
-console.log("trackBarsDuration", trackBars)
-
-console.log("trackBars", typeof(trackBarsLength));
-
-let listOfTrackBars = {};
-
-
+//number of array objects within trackBars
 let numOfArrays = Math.ceil(trackBars.length / 100);
 
-console.log(numOfArrays);
 
+console.log("trackBars", trackBars)
 
-// let getArrayOfBars = function() {
-//   for (i = 0; i < trackBars.length; i++) {
+console.log("trackBars typeOf", typeof(trackBarsLength));
 
-//   }
-// }
+let trackBarsArray = [];
+
+//pushes every bar's duration into a new array
+for (let i = 0; i < trackBarsLength; i++) {
+  let currentBarLength = trackBars[i].duration;
+  trackBarsArray.push(currentBarLength);
+}
+
+console.log(trackBarsArray)
 
 
 // var chartdata = [20, 30, 105, 15, 85, 20, 30, 105, 15, 85, 20, 30, 105, 15, 85, 20, 30, 105, 15, 85];
 
-// var height = 200,
-//     width = 720,
-//     barWidth = 40,
-//     barOffset = 20;
+var height = 200,
+    width = 720,
+    barWidth = 40,
+    barOffset = 20;
 
-// var x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.05);
+var x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.05);
  
-// var yScale = d3.scaleLinear()
-//         .domain([0, d3.max(chartdata)])
-//         .range([0, height]);
+var yScale = d3.scaleLinear()
+        .domain([0, d3.max(trackBarsArray)])
+        .range([0, height]);
 
-// var xScale = d3.scaleLinear()
-//         .domain([0, chartdata.length])
-//         .range([0,width]);
+var xScale = d3.scaleLinear()
+        .domain([0, trackBarsArray.length])
+        .range([0,width]);
 
  
-// d3.select('#bar-chart').append('svg')
-//   .attr('width', width)
-//   .attr('height', height)
-//   .style('background', '#dff0d8')
-//   .selectAll('rect').data(chartdata)
-//   .enter().append('rect')
-//     .style('fill', '#3c763d')
-//     .attr("width", (width/chartdata.length -1))
-//     .attr('height', function (data) {
-//         return yScale(data);
-//     })
-//       .attr('x', function (data, i) {
-//         console.log("xScale(i)", xScale(i));
-//         return xScale(i);
-//     })
-//     .attr('y', function (data) {
-//         console.log("yScale(i)", height-yScale(data));
-//         return height - yScale(data);
-//     });
+d3.select('#bar-chart').append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .style('background', '#dff0d8')
+  .selectAll('rect').data(trackBarsArray)
+  .enter().append('rect')
+    .style('fill', '#3c763d')
+    .attr("width", (width/trackBarsArray.length -1))
+    .attr('height', function (data) {
+        return yScale(data);
+    })
+      .attr('x', function (data, i) {
+        console.log("xScale(i)", xScale(i));
+        return xScale(i);
+    })
+    .attr('y', function (data) {
+        console.log("yScale(i)", height-yScale(data));
+        return height - yScale(data);
+    });
 
 
 });
