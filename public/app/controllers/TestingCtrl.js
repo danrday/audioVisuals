d44e8c5e8c03f9e9c17d325fb1da69279c4f5ecc
Dan Day <danday@gmail.com>
2016-08-02 13:38:23 -0500

    tests to learn scaleBand methods in d3

--- public/app/controllers/TestingCtrl.js
+++ public/app/controllers/TestingCtrl.js
@@ -2,7 +2,7 @@
 
 app.controller("TestingCtrl", function($scope) {
 
-let bardata = [20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80];
+let bardata = [20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80];
 
 
 var height = 200,
@@ -16,11 +16,11 @@ var yScale = d3.scaleLinear()
         .domain([0, d3.max(bardata)])
         .range([0, height]);
 
-var xScale = d3.scaleLinear()
-        .domain([0, bardata.length])
-        .range([0,width]);
+var xScale = d3.scaleBand()
+        .domain(d3.range(0, bardata.length))
+        .range(d3.range(0, width))
+        .paddingInner([.2])
 
- 
 d3.select('#test-chart').append('svg')
   .attr('width', width)
   .attr('height', height)
@@ -28,13 +28,16 @@ d3.select('#test-chart').append('svg')
   .selectAll('rect').data(bardata)
   .enter().append('rect')
     .style('fill', '#3c763d')
-    .attr("width", (width/bardata.length -1))
+    .attr("width", function() {
+      console.log("xScale.bandwidth", xScale.bandwidth()*width)
+      return xScale.bandwidth()*width
+    })
     .attr('height', function (data) {
         return yScale(data);
     })
       .attr('x', function (data, i) {
-        // console.log("xScale(i)", xScale(i));
-        return xScale(i);
+        console.log("xScale(i)", xScale(i));
+        return xScale(i)*width;
     })
     .attr('y', function (data) {
         // console.log("yScale(i)", height-yScale(data));
