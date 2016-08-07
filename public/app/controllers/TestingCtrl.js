14785785eea364b48bc81493e1e657a5aaa478ae
Dan Day <danday@gmail.com>
2016-08-06 21:04:37 -0500

    build stopwatch

--- public/app/controllers/TestingCtrl.js
+++ public/app/controllers/TestingCtrl.js
@@ -3,135 +3,156 @@
 app.controller("TestingCtrl", function($scope) {
 
 
-$scope.time = 0;
+$scope.hundredthSecond = 0;
+
+$scope.second = 0;
+
+$scope.minute = 0;
+
+
 
 let timer = setInterval(function() {
- $scope.time++ 
+ $scope.hundredthSecond++
  $scope.$apply();
- console.log($scope.time)
+
+ if ($scope.hundredthSecond%100 === 0) {
+  if($scope.second === 59) {
+    $scope.second = 0;
+    $scope.minute++;
+    $scope.$apply();
+  } else {
+      $scope.second++
+      $scope.$apply();
+    }
+ }
+
+ console.log($scope.hundredthSecond)
 }, 10)
 
 
 
-let bardata = [20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80];
 
 
-var height = 200,
-    width = 720,
-    barWidth = 40,
-    barOffset = 20;
 
-var x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.05);
+// let bardata = [20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80, 20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80,20, 30, 20, 15, 40, 80];
+
+
+// var height = 200,
+//     width = 720,
+//     barWidth = 40,
+//     barOffset = 20;
+
+// var x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.05);
  
-var yScale = d3.scaleLinear()
-        .domain([0, d3.max(bardata)])
-        .range([0, height]);
+// var yScale = d3.scaleLinear()
+//         .domain([0, d3.max(bardata)])
+//         .range([0, height]);
 
-var xScale = d3.scaleBand()
-        .domain(d3.range(0, bardata.length))
-        .range(d3.range(0, width))
-        .paddingInner([.2])
+// var xScale = d3.scaleBand()
+//         .domain(d3.range(0, bardata.length))
+//         .range(d3.range(0, width))
+//         .paddingInner([.2])
 
-let color1 = '#2ead16';
-let color2 = '#C61C6F';
+// let color1 = '#2ead16';
+// let color2 = '#C61C6F';
 
-var colors = d3.scaleLinear()
-        .domain([d3.min(bardata), d3.max(bardata)])
-        .range([`${color1}`, `${color2}`]);
+// var colors = d3.scaleLinear()
+//         .domain([d3.min(bardata), d3.max(bardata)])
+//         .range([`${color1}`, `${color2}`]);
 
-var colors2 = d3.scaleLinear()
-        .domain([d3.min(bardata), d3.max(bardata)])
-        .range(['#FFFFFF', '#FFB832']); 
+// var colors2 = d3.scaleLinear()
+//         .domain([d3.min(bardata), d3.max(bardata)])
+//         .range(['#FFFFFF', '#FFB832']); 
 
-$scope.colorTest = ""      
+// $scope.colorTest = ""      
 
-$scope.testButton = function () {
-  console.log("buttonpressed");
-  // d3.select('#test-chart').style('background', '#FFFFFF')
-  // d3.selectAll('elementsyouwanttochange').data().enter().style(newcolors)
+// $scope.testButton = function () {
+//   console.log("buttonpressed");
+//   // d3.select('#test-chart').style('background', '#FFFFFF')
+//   // d3.selectAll('elementsyouwanttochange').data().enter().style(newcolors)
 
-  // this works
-  // d3.select('svg').style('background', '#FFFFFF')
+//   // this works
+//   // d3.select('svg').style('background', '#FFFFFF')
 
-  d3.selectAll('rect').style('fill', colors2)
-}
-
-
-
- $scope.$watch('colorTest', function(newVal, oldVal) {
-        if (!newVal) {return};
-
-        // color1 = newVal;
-
-        let colors3 = d3.scaleLinear()
-        .domain([d3.min(bardata), d3.max(bardata)])
-        .range([`${newVal}`, `${color2}`]); 
-
-        // colors;
-
-        d3.selectAll('rect').style('fill', colors3)
-
-    //     color1 = $scope.color;
-    //     d3.selectAll('rect').data(trackAnalysis.bars).enter().style('fill', function(data) {
-    //     return colors(barsDurationFn(data));
-    // });
-});
-
-
- $scope.$watch('colorTest2', function(newVal, oldVal) {
-        if (!newVal) {return};
-
-        color2 = newVal;
-
-        let colors3 = d3.scaleLinear()
-        .domain([d3.min(bardata), d3.max(bardata)])
-        .range([`${color1}`, `${newVal}`]); 
-
-        colors;
-
-        d3.selectAll('rect').style('fill', colors3)
-
-    //     color1 = $scope.color;
-    //     d3.selectAll('rect').data(trackAnalysis.bars).enter().style('fill', function(data) {
-    //     return colors(barsDurationFn(data));
-    // });
-});
-
-
-  $scope.$watch('colorTest3', function(newVal, oldVal) {
-        if (!newVal) {return};
-
-        d3.select('svg').style('background', `${newVal}`)
-
-    //     color1 = $scope.color;
-    //     d3.selectAll('rect').data(trackAnalysis.bars).enter().style('fill', function(data) {
-    //     return colors(barsDurationFn(data));
-    // });
-});
+//   d3.selectAll('rect').style('fill', colors2)
+// }
 
 
 
-d3.select('#test-chart').append('svg')
-  .attr('width', width)
-  .attr('height', height)
-  .style('background', '#dff0d8')
-  .selectAll('rect').data(bardata)
-  .enter().append('rect')
-    .style('fill', colors)
-    .attr("width", function() {
-      console.log("xScale.bandwidth", xScale.bandwidth()*width)
-      return xScale.bandwidth()*width
-    })
-    .attr('height', function (data) {
-        return yScale(data);
-    })
-      .attr('x', function (data, i) {
-        console.log("xScale(i)", xScale(i));
-        return xScale(i)*width;
-    })
-    .attr('y', function (data) {
-        // console.log("yScale(i)", height-yScale(data));
-        return height - yScale(data);
-    });
+//  $scope.$watch('colorTest', function(newVal, oldVal) {
+//         if (!newVal) {return};
+
+//         // color1 = newVal;
+
+//         let colors3 = d3.scaleLinear()
+//         .domain([d3.min(bardata), d3.max(bardata)])
+//         .range([`${newVal}`, `${color2}`]); 
+
+//         // colors;
+
+//         d3.selectAll('rect').style('fill', colors3)
+
+//     //     color1 = $scope.color;
+//     //     d3.selectAll('rect').data(trackAnalysis.bars).enter().style('fill', function(data) {
+//     //     return colors(barsDurationFn(data));
+//     // });
+// });
+
+
+//  $scope.$watch('colorTest2', function(newVal, oldVal) {
+//         if (!newVal) {return};
+
+//         color2 = newVal;
+
+//         let colors3 = d3.scaleLinear()
+//         .domain([d3.min(bardata), d3.max(bardata)])
+//         .range([`${color1}`, `${newVal}`]); 
+
+//         colors;
+
+//         d3.selectAll('rect').style('fill', colors3)
+
+//     //     color1 = $scope.color;
+//     //     d3.selectAll('rect').data(trackAnalysis.bars).enter().style('fill', function(data) {
+//     //     return colors(barsDurationFn(data));
+//     // });
+// });
+
+
+//   $scope.$watch('colorTest3', function(newVal, oldVal) {
+//         if (!newVal) {return};
+
+//         d3.select('svg').style('background', `${newVal}`)
+
+//     //     color1 = $scope.color;
+//     //     d3.selectAll('rect').data(trackAnalysis.bars).enter().style('fill', function(data) {
+//     //     return colors(barsDurationFn(data));
+//     // });
+// });
+
+
+
+// d3.select('#test-chart').append('svg')
+//   .attr('width', width)
+//   .attr('height', height)
+//   .style('background', '#dff0d8')
+//   .selectAll('rect').data(bardata)
+//   .enter().append('rect')
+//     .style('fill', colors)
+//     .attr("width", function() {
+//       console.log("xScale.bandwidth", xScale.bandwidth()*width)
+//       return xScale.bandwidth()*width
+//     })
+//     .attr('height', function (data) {
+//         return yScale(data);
+//     })
+//       .attr('x', function (data, i) {
+//         console.log("xScale(i)", xScale(i));
+//         return xScale(i)*width;
+//     })
+//     .attr('y', function (data) {
+//         // console.log("yScale(i)", height-yScale(data));
+//         return height - yScale(data);
+//     });
 
 });
--- public/app/controllers/TracksCtrl.js
+++ public/app/controllers/TracksCtrl.js
@@ -2,43 +2,43 @@
 
 app.controller("TracksCtrl", function($scope, AuthFactory, TrackStorage, SpotifyFactory) {
 
-$scope.deleteTrack = function(trackID) {
-  TrackStorage.deleteTrack(trackID)
-  .then(function() {
-    TrackStorage.getTracks(AuthFactory.getUser())
-  .then( function (trackCollection) {
-  $scope.trackResults = trackCollection;
-  console.log("tracks", $scope.trackResults);
-   });
+  $scope.deleteTrack = function(trackID) {
+    TrackStorage.deleteTrack(trackID)
+    .then(function() {
+      TrackStorage.getTracks(AuthFactory.getUser())
+    .then( function (trackCollection) {
+    $scope.trackResults = trackCollection;
+    console.log("tracks", $scope.trackResults);
+     });
+    });
+  }
+
+  //  $scope.goToSavedTrack = function(trackId) {
+  //   console.log("track id from tracksCtrl", trackId)
+  //   SpotifyFactory.getSpotifyData(trackId)
+  //   .then(function(resolve) {
+  //     console.log("data", resolve)
+  //   })
+  // }
+
+  $scope.goToSavedTrack = function(trackId) {
+    SpotifyFactory.trackId = trackId
+    console.log("tracks ctrl spotify track id", SpotifyFactory.trackId)
+  }
+
+  $scope.getTrack = function(trackID) {
+    console.log("trackID", trackID)
+  }
+
+  $scope.trackResults = [];
+
+  firebase.auth().onAuthStateChanged(function(){
+   TrackStorage.getTracks(AuthFactory.getUser())
+    .then( function (trackCollection) {
+    $scope.trackResults = trackCollection;
+    console.log("tracks", $scope.trackResults);
+    });
   });
-}
-
-//  $scope.goToSavedTrack = function(trackId) {
-//   console.log("track id from tracksCtrl", trackId)
-//   SpotifyFactory.getSpotifyData(trackId)
-//   .then(function(resolve) {
-//     console.log("data", resolve)
-//   })
-// }
-
-$scope.goToSavedTrack = function(trackId) {
-  SpotifyFactory.trackId = trackId
-  console.log("tracks ctrl spotify track id", SpotifyFactory.trackId)
-}
-
-$scope.getTrack = function(trackID) {
-  console.log("trackID", trackID)
-}
-
-$scope.trackResults = [];
-
-firebase.auth().onAuthStateChanged(function(){
- TrackStorage.getTracks(AuthFactory.getUser())
-  .then( function (trackCollection) {
-  $scope.trackResults = trackCollection;
-  console.log("tracks", $scope.trackResults);
-  });
-});
 
  
   // console.log("tracks", $scope.tracks);
