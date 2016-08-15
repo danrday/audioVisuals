7d7392066215a30add0833bdae8775797235a323
Dan Day <danday@gmail.com>
2016-08-15 15:43:50 -0500

    took private folders off of gitignore

---
+++ public/app/private/firebaseCreds.js
@@ -0,0 +1,8 @@
+"use strict";
+
+app.constant("FBCreds", {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  storageBucket: process.env.storageBucket
+})
